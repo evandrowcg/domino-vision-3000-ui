12001613584959d090c71973f7b7a1286864439a
Evandro Gomes <evandro.wcg@gmail.com>
2025-03-25 17:05:06 -0400

    Fixing gap between halves

--- src/components/Domino/Domino.tsx
+++ src/components/Domino/Domino.tsx
@@ -8,24 +8,30 @@ interface DominoProps {
 }
 
 const Domino: React.FC<DominoProps> = ({ left, right, width, margin = "" }) => {
-  const halfStyle = (value: number, rotate: boolean = false): React.CSSProperties => ({
-    flex: 1,
+  const halfStyle = (
+    value: number,
+    isRightHalf: boolean = false
+  ): React.CSSProperties => ({
+    width: "50%",
+    height: "100%",
     backgroundImage: `url(/images/domino_half_${value}.png)`,
     backgroundSize: "cover",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
-    transform: rotate ? "rotate(180deg)" : undefined,
+    transform: isRightHalf ? "rotate(180deg)" : undefined,
+    marginLeft: isRightHalf ? "-1px" : "0", // Overlap right half slightly
+    boxSizing: "border-box",
   });
 
   return (
     <div
       style={{
         width,
-        aspectRatio: "2 / 1", // horizontal domino: width is 2x height
+        aspectRatio: "2 / 1", // Horizontal domino: width is 2x height
         display: "flex",
         float: "left",
         margin: margin,
-        flexDirection: "row", // side-by-side
+        flexDirection: "row",
         overflow: "hidden",
       }}
     >
